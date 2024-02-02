97f04103fbe94d1849b8ba785ee30ecf8650762e
RITIK KUMAR <93565586+TheRitik@users.noreply.github.com>
2024-02-02 18:38:58 +0000

    conversion of datatypes

---
+++ Js/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score = "33abs"
+
+let score1 = null
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);  //Not a number NaN
+
+console.log(valueInNumber1);
