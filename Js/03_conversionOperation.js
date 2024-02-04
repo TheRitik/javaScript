4eb95fb276a2b48f18fe9e18d9b3d2cccf5b827f
RITIK KUMAR <93565586+TheRitik@users.noreply.github.com>
2024-02-04 07:38:16 +0000

    Conversion done

--- Js/03_conversionOperation.js
+++ Js/03_conversionOperation.js
@@ -9,3 +9,55 @@ console.log(typeof valueInNumber);
 console.log(valueInNumber);  //Not a number NaN
 
 console.log(valueInNumber1);
+
+
+/*****           OPERATIONS         *****/
+
+let value = 3
+let negValue = -value
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);   // power
+console.log(2/2);
+console.log(2%2);*/
+
+let str1 = "Hello"
+let str2 = " Ritik"
+console.log(str1 + str2 );
+
+/*console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+3);    // bcoz treats all elements as string
+console.log(1+2+"3");*/
+
+console.log("2">1); // automatically convert into number 
+
+/*console.log(null == 0); // false
+console.log(null >= 0); // true : comparison convert null to a number and treats null as 0 */
+
+
+// === does not convert 
+// console.log("2" === 2); //false 
+
+const bigNumber = 23142353145324513453452345345n
+
+
+// Reference (Non Primitive)
+// Object, Array, Function
+
+const heros = ["Shaktiman","Naagraj"]
+
+let myobj = {
+    age: 14,
+    name: "ritik",
+}
+
+let func = function(){
+    console.log("Hello");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof func);
+console.log(typeof myobj);
